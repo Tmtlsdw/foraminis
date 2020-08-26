c3a329614a4e801a209db3df0b407eeed7d99820
Rodrigue Coail <rodrigue.coail@tmtlsdw.com>
2020-08-26 21:13:46 +0200

    changing files

--- src/Foraminis.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import Foraminis from "./containers/Foraminis";
-
-test('renders learn react link', () => {
-  const { getByText } = render(<Foraminis />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
--- src/constants.js
+++
@@ -1,1 +0,0 @@
-export const BLABLA = "BLABLA";
